9e9e822e081d5f2bd3dd1025cd1633bdd738dff5
Nick-Vyvyan <vyvyannick@gmail.com>
2024-06-12 15:53:44 -0700

    Create Lose Screen on Player Death

--- Source/SimpleShooter/SimpleShooterPlayerController.cpp
+++ Source/SimpleShooter/SimpleShooterPlayerController.cpp
@@ -3,10 +3,17 @@
 
 #include "SimpleShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint//UserWidget.h"
 
 void ASimpleShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
 
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
+
+	UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+	if (LoseScreen)
+	{
+		LoseScreen->AddToViewport();
+	}
 }
